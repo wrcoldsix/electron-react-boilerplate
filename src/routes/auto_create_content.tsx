79c19f6d40aedd8590481062abcc1e5119cfb4e8
weiruicong <weiruicong@egridcloud.com>
2024-03-05 18:19:31 +0800

    第二次提交

--- src/routes/auto_create_content.tsx
+++ src/routes/auto_create_content.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react"
 import './auto_create_content.scss'
+import ExportExcel from "../utils/createExcel";
 
 const AutoCreatContent = ()=>{
-  const [promptStr,setPromptStr] = useState('')
+  const radioList = [
+    {label:'生成方式1',value:'0'},
+    {label:'生成方式2',value:'1'},
+  ]
+  const [radioVal,setradioVal] = useState('')
+  const [promptStr,setPromptStr] = useState('你是地理专家，请用【】为标题，写一篇1500字的文章。')
   const [title,setTitle] = useState('%$跟%$哪个更有发展潜力？这篇文章告诉你答案！')
   const [displayMaterial,setDisplayMaterial] = useState(false)
   const [materialList,setMaterialList] = useState('苏州，上海，山东;嘉定，潍坊')  // ;分隔
@@ -30,10 +36,41 @@ const AutoCreatContent = ()=>{
     setMaterialList(val)
   }
 
+  // 递归整理
+
+  // 排列组合
+  const combine = (data:string[])=>{
+    const finallyData:Array<string[]> = []
+    const firstDataArr = data[0].replaceAll('，',',').split(',')
+    firstDataArr.forEach(item=>{
+      let strArr:any[] = []
+      data.forEach((cont,ind)=>{
+        if (ind>0){
+          let index = 0
+          const materialStr = cont.replaceAll('，',',')
+          const materialStrArr = materialStr.split(',')
+          for(let i = index;i<materialStrArr.length;i++){
+            strArr = []
+            index=i
+            if (strArr.length===data.length) {
+              break;
+            }
+            strArr.push(item)
+            strArr.push(materialStrArr[index])
+            finallyData.push(strArr)
+          }
+        }
+      })
+    })
+
+    return finallyData
+  }
+
   // 生成
   const generate = ()=>{
     const allTitleArr = title.split('%$')
     let allMaterialList:string[] = []
+    // 如果有4个变量的，这种特殊处理，后两个跟前两个是一致的
     if (materialList.split(';').length+1!==allTitleArr.length){
       allTitleArr.forEach(item=>{
         allMaterialList.push(materialList+';')
@@ -42,61 +79,78 @@ const AutoCreatContent = ()=>{
       allMaterialList = materialList.split(';')
     }
 
-    let titleStr = ''
-    const finallyTitleArr = title.split('%$').map(item=>{
-      titleStr += item
-      allMaterialList.forEach((cont,index)=>{
-        const materialStr = cont.replaceAll('，',',')
-        const materialStrArr = materialStr.split(',')
-        for(let i=index;i<materialStrArr.length;i++){
-          titleStr += materialStrArr[i]
-        console.log('titleStr1',titleStr);
-          break;
-        }
+    const materialCombineData = combine(allMaterialList)
+    const finallyTitleArr = materialCombineData.map(item=>{
+      let titleStr = title
+      item.forEach(cont=>{
+        titleStr = titleStr.replace('%$',cont)
       })
 
       return titleStr
     })
+
+    const finallyPromptArr = finallyTitleArr.map(item=>{
+      let prompt = promptStr
+      return   prompt.replace('【】',item)
+    })
+
+    ExportExcel([[...finallyPromptArr]])
   }
 
   return (
-    <div className="main-box">
-      <div className="cont-box">
-        <div className="cont-title">完整提示词:</div>&nbsp;&nbsp;
-        <textarea
-          cols={30}
-          rows={10}
-          value={promptStr}
-          onChange={handlePromptChange}
-        ></textarea>
+    <div className="main">
+      <div className="main-box">
+        <div className="cont-box">
+          <div className="cont-title">完整提示词:</div>&nbsp;&nbsp;
+          <textarea
+            cols={50}
+            rows={10}
+            value={promptStr}
+            onChange={handlePromptChange}
+          ></textarea>
+        </div>
       </div>
-      <div className="cont-box">
-        <div className="cont-title">标题输入:</div>&nbsp;&nbsp;
-        <textarea
-          cols={30}
-          rows={10}
-          value={title}
-          onChange={handleTitleChange}
-        ></textarea>
+      <div className="main-box">
+        {
+          radioList.map(item=>{
+            return (
+              <span key={item.value}>
+                <label>{item.label}</label>
+                <input type="radio" name="test" id=""  value={item.value}/>
+              </span>
+            )
+          })
+        }
       </div>
-      <button onClick={handleMaterialClick}>素材提供</button>
-      {
-        displayMaterial && (
-          <>
-          <div className="cont-box">
-            <div className="cont-title">素材内容:</div>&nbsp;&nbsp;
-            <textarea
-              cols={30}
-              rows={10}
-              value={materialList}
-              onChange={handleMaterialChange}
-            ></textarea>
-          </div>
-          {/* 暂时使用按钮，生成内容，最后提供导出 */}
-          <button onClick={generate}>最终生成</button>
-          </>
-        )
-      }
+      <div className="main-box">
+        <div className="cont-box">
+          <div className="cont-title">标题输入:</div>&nbsp;&nbsp;
+          <textarea
+            cols={30}
+            rows={10}
+            value={title}
+            onChange={handleTitleChange}
+          ></textarea>
+        </div>
+      </div>
+        <button onClick={handleMaterialClick}>素材提供</button>
+        {
+          displayMaterial && (
+            <>
+              <div className="cont-box">
+                <div className="cont-title">素材内容:</div>&nbsp;&nbsp;
+                <textarea
+                  cols={30}
+                  rows={10}
+                  value={materialList}
+                  onChange={handleMaterialChange}
+                ></textarea>
+              </div>
+              {/* 暂时使用按钮，生成内容，最后提供导出 */}
+              <button onClick={generate}>最终生成文件</button>
+            </>
+          )
+        }
     </div>
   )
 }
---
+++ src/utils/createExcel.tsx
@@ -0,0 +1,55 @@
+const XLSX  = require('xlsx')
+
+/**
+ * @data 表格数据 data = [{}] 默认为[[]] 数据结构 二维数组
+ * @ExcelTitle 表格标题 默认 [ '模板Excel' ]
+ * @ExcelHeader 表头信息 默认 []
+ * @sheetName sheet（表）名称 默认 ‘学生信息’
+ * @ExcelName 文件名称 默认 ‘学生信息模板’
+ */
+const ExportExcel = (
+    data:any[] = [[]],
+    ExcelTitle = [ '所有提示词' ],
+    ExcelHeader = [],
+    sheetName = '所有提示词',
+    ExcelName = '所有提示词'
+) => {
+
+    // (1)将数据源转成我们需要的二维数组
+    const body = data
+
+    // (2)定义表头
+    const header = [ ExcelTitle, ExcelHeader ]
+
+    // (3)将定义好的表头添加到 body 中
+    body.unshift( ...header )
+
+    // (4)创建虚拟的 workbook
+    const workbook = XLSX.utils.book_new()
+
+    // (5)将二维数组转成 sheet
+    const sheet = XLSX.utils.aoa_to_sheet( body )
+
+    // (6)设置单元格合并 { s: { r: 0, c: 0 }, e: { r: 0, c: 4 } }, 即为一个规则，s:开始位置, e:结束位置, r:行, c:列
+    // 可根据实际情况调整，此处偷懒，没有想很复杂表格
+    const merges = [
+        { s: { r: 0, c: 0 }, e: { r: 0, c: ExcelHeader.length - 1 } },
+    ]
+
+    // (7)将merges添加到sheet中，设置合并单元格
+    sheet[ '!merges' ] = merges
+
+    // (8)设置行列宽高并添加到sheet中
+    const cols = [ { wch: 20 }, { wch: 20 }, { wch: 20 }, { wch: 20 }, { wch: 20 } ]
+    const rows = [ { hpx: 30 } ]
+    sheet[ '!cols' ] = cols
+    sheet[ '!rows' ] = rows
+
+    // (9)向workbook中添加 sheet
+    XLSX.utils.book_append_sheet( workbook, sheet, sheetName  )
+
+    // (10)导出Excel
+    XLSX.writeFile( workbook, ExcelName + '.xlsx' )
+}
+
+export default ExportExcel
